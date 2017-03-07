41851e1728952f24c841175ab03abef4c993c93a
Arturo Parra <arturoteleco95@protonmail.ch>
2017-03-07 13:32:08 +0100

    Añadido(NO COMPILADO) descubrimiento(NO TERMINADO)

--- serverUDP/libs/udp.hpp
+++ serverUDP/libs/udp.hpp
@@ -15,39 +15,70 @@
 namespace octopus{
 
     class octoUDPserver : public octoNet{
-    private:
-        UDPserver_t* discoverSock;
-        UDPserver_t* publisherSock;
-        UDPserver_t* suscriberSock;
 
     public:
         octoUDPserver (new_configIPv4_t* nw = defaultNetConfig()) : octoNet(nw){
 
             //this->changeNetConfig(nw);
 
-            auto& tmp = this->inet->udp().bind(5085);
+            auto& tmp = this->inet->udp().bind(DISCOVER_PORT);
 
             discoverSock = &tmp;
-            printf("Server listening on discover port 5085 \n");
+            printf("Server listening on discover port %d \n", DISCOVER_PORT);
 
-            auto& tmp2 = this->inet->udp().bind(5065);
+            auto& tmp2 = this->inet->udp().bind(PUBLISHER_PORT);
 
             publisherSock = &tmp2;
-            printf("Server listening on publisher port 5065 \n");
+            printf("Server listening on publisher port %d \n", PUBLISHER_PORT);
 
-            auto& tmp3 = this->inet->udp().bind(5075);
+            auto& tmp3 = this->inet->udp().bind(SUSCRIBER_PORT);
 
             suscriberSock = &tmp3;
-            printf("Server listening on suscriber port 5075 \n");
+            printf("Server listening on suscriber port %d \n", SUSCRIBER_PORT);
 
         }
 
+        void run(){
 
+            trumpetServer();
+
+            discoverSock.on_read(
+              [&sock](
+                UDP::addr_t addr,
+                UDP::port_t port,
+                const char* data,
+                size_t len
+              )
+              {
+                 std::string strdata(data, len);
+                 CHECK(1, "Getting UDP data from %s:  %d -> %s",
+                       addr.str().c_str(), port, strdata.c_str());
+               }
+
+
+        }
 
 
         virtual ~octoUDPserver (){
 
         }
+
+    private:
+        UDPserver_t* discoverSock;
+        UDPserver_t* publisherSock;
+        UDPserver_t* suscriberSock;
+
+
+        void trumpetServer(){
+
+            auto myIP = this->getIP();
+            auto broadcast = this->getBROADCAST();
+
+            size_t myIP_l = std::strlen(myIP);
+
+            discoverSock->sendto(broadcast, DISCOVER_PORT, myIP, myIP_l);
+        }
+
     };
 
 
