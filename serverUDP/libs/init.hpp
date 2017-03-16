c5626722b91e5be1b90cdb1d29d4e1ce5bfe6b7d
Arturo Parra <arturoteleco95@protonmail.ch>
2017-03-16 13:09:18 +0100

    Hecho los TODO, para pasar las direccion ip como string y por argumentos al lanzarlos

--- serverUDP/libs/init.hpp
+++ serverUDP/libs/init.hpp
@@ -12,8 +12,13 @@
 
 
 //Librerias de IncludeOS
+#include <os>
 #include <net/inet4>
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stack>
 
 using namespace std;
 
@@ -50,6 +55,78 @@ namespace octopus{
 
 
     //TODO: Una funcion que le pases la IP, la NETMASK, el GATEWAY y el DNS en formato string y te devuelva un tipo new_configIPv4_t
+
+    new_configIPv4_t* formatStringConfig(const char* ip, const char* netmask, const char* gateway, const char* dns){
+        /*
+        std::cout << "IP -> " << ip << std::endl;
+        std::cout << "NETMASK -> " << netmask << std::endl;
+        std::cout << "GATEWAY -> " << gateway << std::endl;
+        std::cout << "DNS -> " << dns << std::endl;*/
+        //Devuelve un in_addr_t que es como un uint32_t
+        auto ipf = inet_addr(ip);
+        auto ntf = inet_addr(netmask);
+        auto gtf = inet_addr(gateway);
+        auto dnsf = inet_addr(dns);
+
+        static new_configIPv4_t* NETWORK_CONFIG = new new_configIPv4_t;
+
+        NETWORK_CONFIG->ip[3] = ipf >> 24;
+        NETWORK_CONFIG->ip[2] = (ipf >> 16) & 0xFF;
+        NETWORK_CONFIG->ip[1] = (ipf >> 8) & 0xFF;
+        NETWORK_CONFIG->ip[0] = ipf & 0xFF;
+
+        NETWORK_CONFIG->netmask[3] = ntf >> 24;
+        NETWORK_CONFIG->netmask[2] = (ntf >> 16) & 0xFF;
+        NETWORK_CONFIG->netmask[1] = (ntf >> 8) & 0xFF;
+        NETWORK_CONFIG->netmask[0] = ntf & 0xFF;
+
+        NETWORK_CONFIG->gateway[3] = gtf >> 24;
+        NETWORK_CONFIG->gateway[2] = (gtf >> 16) & 0xFF;
+        NETWORK_CONFIG->gateway[1] = (gtf >> 8) & 0xFF;
+        NETWORK_CONFIG->gateway[0] = gtf & 0xFF;
+
+        NETWORK_CONFIG->dns[3] = dnsf >> 24;
+        NETWORK_CONFIG->dns[2] = (dnsf >> 16) & 0xFF;
+        NETWORK_CONFIG->dns[1] = (dnsf >> 8) & 0xFF;
+        NETWORK_CONFIG->dns[0] = dnsf & 0xFF;
+
+        return NETWORK_CONFIG;
+
+    }
+
+    std::stack<std::string> parseArgs(const std::string &args){
+
+        unsigned int start = 0, end;
+
+        std::stack<std::string> result;
+
+        std::string delimeter = " ";
+
+        bool nombre_ejecutable = false;
+
+        while((end = args.find(delimeter, start)) != std::string::npos){
+            if(!nombre_ejecutable){
+                nombre_ejecutable = true;
+            }else{
+                unsigned int tmp = args.find(delimeter, start);
+                if((tmp >= end) || (tmp == std::string::npos)){
+
+                    std::string tmp = args.substr(start, end - start);
+                    result.push(tmp);
+                }
+
+
+            }
+            start = end + 1;
+        }
+
+        if(start < (args.length() - 1)){
+            std::string tmp = args.substr(start, args.length() - start);
+            result.push(tmp);
+        }
+
+        return result;
+    }
 
 }
 
