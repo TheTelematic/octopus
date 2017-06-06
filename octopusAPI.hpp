ba53f2c08040b7a8d342aa4f21007f91c0c846d5
Arturo Parra <arturoteleco95@protonmail.ch>
2017-06-06 23:02:39 +0200

    Started the high level API

---
+++ octopusAPI.hpp
@@ -0,0 +1,46 @@
+/*
+
+    This is the API of my service, for future developers
+
+*/
+#ifndef __OCTOPUSAPI__HPP__
+#define __OCTOPUSAPI__HPP__
+
+#include "libs/udp.hpp"
+#include "libs/types.hpp"
+#include "libs/myapi.hpp"
+#include "libs/octopus.hpp"
+
+
+
+namespace octopus{
+    class octopusAPI {
+    private:
+        Octopus master_server;
+
+
+    public:
+        octopusAPI (){
+            master_server.configDiscovery();
+
+            master_server.configAnnouncement();
+
+            master_server.configSuscription();
+
+            master_server.configPublishment();
+
+            master_server.configShowTableDiscoveredServers();
+
+            master_server.configShowTopics();
+        }
+
+        void create_topic(topic_t topic);
+        void suscribe_to_topic(topic_t topic);
+        void publish(Message message);
+
+
+
+    };
+}
+
+#endif
